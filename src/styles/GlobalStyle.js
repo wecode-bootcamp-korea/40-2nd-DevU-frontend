0e597afcee9baccc88ec3cc6090c9f9047f14a60
Hyobin Bae <amy@Hyobinui-MacBookPro.local>
2022-12-26 09:21:22 +0900

    Add: MyLibrary_layout 구현 완료

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -4,14 +4,14 @@ import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
 ${reset}
 
- * {
+* {
   box-sizing: border-box;
 }
 
 body {
   font-family:'Noto Sans KR', sans-serif;
-
 }
+
 `;
 
 export default GlobalStyle;
