ca741f9700c7a3d43dd5c4e009f69774fad3a705
Hyobin Bae <amy@Hyobinui-MacBookPro.local>
2022-12-26 20:34:13 +0900

    Modify: order layout 리뷰 반영

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 import variables from './styles/variables';
+
 import Router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
