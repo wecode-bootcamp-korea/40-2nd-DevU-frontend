8dc4ab72714d605c22e2520b4168699b90415aca
Hyobin Bae <amy@Hyobinui-MacBookPro.local>
2023-01-03 11:31:57 +0900

    Modify:뷰어 url 불러오기 기능 수정

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import theme from './styles/theme';
 import variables from './styles/variables';
 
 import Router from './Router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, far, fas);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
